5e4d9c223af517364e511c4ea1ef2038344ba23c
Ahmad Almasri <ahmed.almasri@ymail.com>
2020-05-01 02:21:08 +0300

    add base bundle path plist ROOT

---
+++ FBSnapshotTestCase/BaseSnapshotTestCase.swift
@@ -0,0 +1,30 @@
+//
+//  BaseSnapshotTestCase.swift
+//  FBSnapshotTestCase iOS
+//
+//  Created by Ahmad Almasri on 5/1/20.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+import Foundation
+
+public class BaseSnapshotTestCase: FBSnapshotTestCase {
+	
+	var rootPath: String {
+		guard let root = Bundle(for: Self.self).object(forInfoDictionaryKey: "ROOT") as? String
+			else {
+				fatalError("the `ROOT` key not added to info.plist")
+		}
+		return "\(root)\(Self.self)"
+	}
+	
+	public override func getReferenceImageDirectory(withDefault dir: String?) -> String {
+		
+		return "\(rootPath)/ReferenceImages"
+	}
+	
+	public override func getImageDiffDirectory(withDefault dir: String?) -> String {
+				
+		return "\(rootPath)/FailureDiffs"
+	}
+}
